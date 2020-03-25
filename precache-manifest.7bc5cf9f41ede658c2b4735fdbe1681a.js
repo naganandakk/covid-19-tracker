896255f890f9f4b769101150edf5b9e99053a2b0
Nagananda K K <naganandakk@gmail.com>
2020-03-25 20:20:42 +0530

    Updates

---
+++ precache-manifest.7bc5cf9f41ede658c2b4735fdbe1681a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc7c0690cebfb72b4d6c94959e2452a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "4259c6ae00123838f025",
+    "url": "/covid-19-tracker/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2fa49c95c865d2262952",
+    "url": "/covid-19-tracker/static/css/main.73e5f796.chunk.css"
+  },
+  {
+    "revision": "4259c6ae00123838f025",
+    "url": "/covid-19-tracker/static/js/2.0b7d5d64.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/covid-19-tracker/static/js/2.0b7d5d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa49c95c865d2262952",
+    "url": "/covid-19-tracker/static/js/main.f93c5a24.chunk.js"
+  },
+  {
+    "revision": "c7ee893204412e748ec1",
+    "url": "/covid-19-tracker/static/js/runtime-main.72171ba5.js"
+  }
+]);
