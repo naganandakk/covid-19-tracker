686c9a17b85605bff43cc679db5045ab4cfd0e6b
Nagananda K K <naganandakk@gmail.com>
2020-03-25 23:52:45 +0530

    Updates

---
+++ precache-manifest.1e794ca953e5330aae7fb82030d60e73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9602337efddb85e8fbc66c5cf0b1aac",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "463b5204b542710b1f38",
+    "url": "/covid-19-tracker/static/css/2.66cc8250.chunk.css"
+  },
+  {
+    "revision": "2c6b52926139c227cb31",
+    "url": "/covid-19-tracker/static/css/main.044231c1.chunk.css"
+  },
+  {
+    "revision": "463b5204b542710b1f38",
+    "url": "/covid-19-tracker/static/js/2.195a5132.chunk.js"
+  },
+  {
+    "revision": "a51ff95b3f79621f2472576f8fd57509",
+    "url": "/covid-19-tracker/static/js/2.195a5132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6b52926139c227cb31",
+    "url": "/covid-19-tracker/static/js/main.dcafe1f2.chunk.js"
+  },
+  {
+    "revision": "c7ee893204412e748ec1",
+    "url": "/covid-19-tracker/static/js/runtime-main.72171ba5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-tracker/precache-manifest.f1689077ef8fc2ff69bd21c91ae7d58a.js"
+  "/covid-19-tracker/precache-manifest.1e794ca953e5330aae7fb82030d60e73.js"
 );
 
 self.addEventListener('message', (event) => {
